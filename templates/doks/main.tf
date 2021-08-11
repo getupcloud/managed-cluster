b41f46abdc8ea406862cfc3a6c33a032af5a98cf
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-08-11 10:41:21 -0300

    Missing DO vars

--- templates/doks/main.tf
+++ templates/doks/main.tf
@@ -1,12 +1,10 @@
 module "doks" {
   source                = "github.com/getupcloud/terraform-cluster-doks?ref=main"
   name                  = var.name
-  do_token              = var.
+  do_token              = var.do_token
   region                = var.region
-  aws_secret_access_key = var.aws_secret_access_key
-  vpc_id                = var.vpc_id
-  subnet_ids            = var.subnet_ids
-  node_groups           = var.node_groups
-  node_groups_defaults  = var.node_groups_defaults
+  vpc_uuid              = var.vpc_uuid
+  node_pool             = var.node_pool
+  node_pools            = var.node_pools
   flux_git_repo         = var.flux_git_repo
 }
