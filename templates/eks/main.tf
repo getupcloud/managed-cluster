81cf7d13a543b64f639c09de5d8955bf41b95492
Lanay M <marqueslanay@gmail.com>
2021-08-31 19:22:28 -0300

    add vars auth and tags

--- templates/eks/main.tf
+++ templates/eks/main.tf
@@ -10,4 +10,7 @@ module "eks" {
   node_groups_defaults  = var.node_groups_defaults
   flux_git_repo         = var.flux_git_repo
   s3_buckets            = var.s3_buckets
+  auth_iam_users        = var.auth_iam_users
+  auth_iam_roles        = var.auth_iam_roles
+  tags                  = var.tags
 }
