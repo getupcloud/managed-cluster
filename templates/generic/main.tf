e8bb1eba623b686191327b412be27b6ec5bcb59b
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-11-04 19:40:23 -0300

    minor changes

--- templates/generic/main.tf
+++ templates/generic/main.tf
@@ -1,7 +1,9 @@
 module "cluster" {
   source = "github.com/getupcloud/terraform-cluster-generic?ref=main"
 
-  name                = var.name
-  flux_git_repo       = var.flux_git_repo
-  kubeconfig_filename = var.kubeconfig_filename
+  name                   = var.name
+  flux_git_repo          = var.flux_git_repo
+  kubeconfig_filename    = var.kubeconfig_filename
+  cronitor_api_key       = var.cronitor_api_key
+  cronitor_pagerduty_key = var.cronitor_pagerduty_key
 }
--- templates/kubespray/main.tf
+++ templates/kubespray/main.tf
@@ -1,8 +1,10 @@
 module "cluster" {
   source = "github.com/getupcloud/terraform-cluster-kubespray?ref=main"
 
-  name                = var.name
-  kubespray_git_ref   = var.kubespray_git_ref
-  flux_git_repo       = var.flux_git_repo
-  kubeconfig_filename = var.kubeconfig_filename
+  name                   = var.name
+  flux_git_repo          = var.flux_git_repo
+  kubeconfig_filename    = var.kubeconfig_filename
+  kubespray_git_ref      = var.kubespray_git_ref
+  cronitor_api_key       = var.cronitor_api_key
+  cronitor_pagerduty_key = var.cronitor_pagerduty_key
 }
