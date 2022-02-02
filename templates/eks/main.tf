40892add64740a6e4bb6262e24aba5e80e429d7a
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-02-02 12:50:29 -0300

    Bump module cluster-eks

--- templates/eks/main.tf
+++ templates/eks/main.tf
@@ -1,5 +1,5 @@
 module "eks" {
-  source = "github.com/getupcloud/terraform-cluster-eks?ref=v1.0"
+  source = "github.com/getupcloud/terraform-cluster-eks?ref=v1.1"
 
   auth_iam_roles               = var.auth_iam_roles
   auth_iam_users               = var.auth_iam_users
--- templates/eks/providers-eks.tf
+++ templates/eks/providers-eks.tf
@@ -3,8 +3,3 @@ provider "aws" {
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
-
-provider "kubectl" {
-  config_path       = var.kubeconfig_filename
-  apply_retry_count = 2
-}
