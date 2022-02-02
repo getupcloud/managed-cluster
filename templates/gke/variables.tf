d06364984dc6088a05b9c6deca82778fa2fde3e8
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-02-02 20:31:45 -0300

    gke: cleanup

--- templates/gke/variables.tf
+++ templates/gke/variables.tf
@@ -14,66 +14,16 @@ variable "customer_name" {
   type        = string
 }
 
-variable "flux_git_repo" {
-  description = "GitRepository URL"
-  type        = string
-  default     = ""
-}
-
-variable "flux_version" {
-  description = "Flux version to install"
-  type        = string
-  default     = "v0.15.3"
-}
-
-variable "cronitor_api_key" {
-  description = "Cronitor API key. Leave empty to destroy"
-  type        = string
-  default     = ""
-}
-
-variable "cronitor_pagerduty_key" {
-  description = "Cronitor PagerDuty key"
-  type        = string
-  default     = ""
-}
-
 variable "api_endpoint" {
   description = "Kubernetes API endpoint (Informative only)"
   type        = string
   default     = ""
 }
 
-variable "manifests_path" {
-  description = "Manifests dir inside GitRepository"
-  type        = string
-  default     = ""
-}
-
-variable "manifests_template_vars" {
-  description = "Template vars for use by cluster manifests"
-  type        = any
-  default = {
-    alertmanager_pagerduty_key : ""
-  }
-}
-
-variable "kubeconfig_filename" {
-  description = "Kubeconfig path"
-  type        = string
-  default     = "~/.kube/config"
-}
-
 variable "get_kubeconfig_command" {
   description = "Command to create/update kubeconfig"
   type        = string
   default     = "true"
-}
-
-variable "flux_wait" {
-  description = "Wait for all manifests to apply"
-  type        = bool
-  default     = true
 }
 
 variable "project_id" {
