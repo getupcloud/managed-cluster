c94bed34c772ead469433fce69c78128daee03d5
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-03-14 19:02:38 -0300

    gke: bump

--- templates/gke/main.tf
+++ templates/gke/main.tf
@@ -1,5 +1,5 @@
 module "gke" {
-  source = "github.com/getupcloud/terraform-cluster-gke?ref=v1.6"
+  source = "github.com/getupcloud/terraform-cluster-gke?ref=v1.7"
 
   api_endpoint                = var.api_endpoint
   cluster_autoscaling         = var.cluster_autoscaling
