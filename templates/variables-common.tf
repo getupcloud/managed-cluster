1c689995c0e130f9485299238dc8ac96222eb606
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-03-15 14:11:47 -0300

    Add var use_kubeconfig

--- templates/variables-common.tf
+++ templates/variables-common.tf
@@ -70,6 +70,12 @@ variable "cronitor_pagerduty_key" {
   default     = ""
 }
 
+variable "use_kubeconfig" {
+  description = "Should kubernetes/kubectl providers use local kubeconfig or credentials from cloud module"
+  type        = bool
+  default     = false
+}
+
 variable "kubeconfig_filename" {
   description = "Kubeconfig path"
   type        = string
