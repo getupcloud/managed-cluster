ac5fa7a241a3f3a164c88a850ace2e49dd12f266
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-05-24 11:42:38 -0300

    aks: udpate provider credentials config

--- templates/aks/providers-aks.tf
+++ templates/aks/providers-aks.tf
@@ -4,21 +4,37 @@ provider "azurerm" {
   tenant_id                  = var.tenant_id
   client_id                  = var.client_id
   client_secret              = var.client_secret
+
   features {}
 }
 
 # Configure the Azure Resource Manager Provider for private DNS zones subscription
 provider "azurerm" {
-  alias           = "private_dns_zone"
-  subscription_id = var.private_dns_zone_subscription_id == "" ? var.subscription_id : var.private_dns_zone_subscription_id
+  alias = "private_dns_zone"
+
+  skip_provider_registration = true
+  subscription_id            = var.private_dns_zone_subscription_id == "" ? var.subscription_id : var.private_dns_zone_subscription_id
+  tenant_id                  = var.tenant_id
+  client_id                  = var.client_id
+  client_secret              = var.client_secret
 
   features {}
 }
 
 # Configure the Azure Resource Manager Provider for Azure container registry subscription
 provider "azurerm" {
-  alias           = "acr"
-  subscription_id = var.acr_subscription_id == "" ? var.subscription_id : var.acr_subscription_id
+  alias = "acr"
+
+  skip_provider_registration = true
+  subscription_id            = var.acr_subscription_id == "" ? var.subscription_id : var.acr_subscription_id
+  tenant_id                  = var.tenant_id
+  client_id                  = var.client_id
+  client_secret              = var.client_secret
 
   features {}
+}
+
+provider "azuread" {
+  tenant_id = var.tenant_id
+  client_id = var.client_id
 }
