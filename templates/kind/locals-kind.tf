81ac449504c6a5ddaf7f5aa7e4cedcab40231d36
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-06-15 10:53:14 -0300

    add dlocals.cluster_type

---
+++ templates/kind/locals-kind.tf
@@ -0,0 +1,3 @@
+locals {
+  cluster_type = "kind"
+}
--- templates/locals.tf
+++ templates/locals.tf
@@ -2,6 +2,7 @@ locals {
   manifests_template_vars = merge({
     cluster_name : var.name
     cluster_sla : var.sla
+    cluster_type : local.cluster_type
     customer_name : var.customer
   }, var.manifests_template_vars)
 }
