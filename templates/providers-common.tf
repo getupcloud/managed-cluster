60a6e40c8b3ca7f36bf758e832bde1ccee416250
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-07-04 13:16:08 -0300

    Move providers into toplevel

--- templates/providers-common.tf
+++ templates/providers-common.tf
@@ -1,3 +1,11 @@
+provider "cronitor" {
+  api_key = var.cronitor_api_key
+}
+
+provider "opsgenie" {
+  api_key = var.opsgenie_api_key
+}
+
 provider "shell" {
   enable_parallelism = true
   interpreter        = ["/bin/bash", "-c"]
