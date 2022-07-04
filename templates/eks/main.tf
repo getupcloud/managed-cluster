5faa8fc6157433b3f6785e7f1cb480d992a82cd7
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-07-04 12:03:01 -0300

    bump eks

--- templates/eks/main.tf
+++ templates/eks/main.tf
@@ -1,5 +1,5 @@
 module "eks" {
-  source = "github.com/getupcloud/terraform-cluster-eks?ref=v1.43"
+  source = "github.com/getupcloud/terraform-cluster-eks?ref=v1.44"
 
   account_id                   = var.account_id
   auth_iam_roles               = var.auth_iam_roles
