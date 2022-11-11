0f14e2095dcd0a0576e3f049613cc1c8694b7820
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-11-11 18:59:51 -0300

    WIP

--- templates/aks/main.tf
+++ templates/aks/main.tf
@@ -1,5 +1,5 @@
 module "aks" {
-  source = "github.com/getupcloud/terraform-cluster-aks?ref=v2.0.0-alpha4"
+  source = "github.com/getupcloud/terraform-cluster-aks?ref=v2.0.0-alpha5"
 
   # cluster basics
   customer_name  = var.customer_name
