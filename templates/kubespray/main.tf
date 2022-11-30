fc94a9f5875fc9762fc9eb15fa67bc21f7097450
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-11-30 15:14:25 -0300

    bump

--- templates/kubespray/main.tf
+++ templates/kubespray/main.tf
@@ -1,5 +1,5 @@
 module "cluster" {
-  source = "github.com/getupcloud/terraform-cluster-kubespray?ref=v3.0.0-alpha7"
+  source = "github.com/getupcloud/terraform-cluster-kubespray?ref=v3.0.0-alpha8"
 
   # cluster basics
   customer_name    = var.customer_name
--- templates/okd/main.tf
+++ templates/okd/main.tf
@@ -1,5 +1,5 @@
 module "cluster" {
-  source = "github.com/getupcloud/terraform-cluster-generic?ref=v2.0.0-alpha12"
+  source = "github.com/getupcloud/terraform-cluster-generic?ref=v2.0.0-alpha13"
 
   # cluster basics
   customer_name    = var.customer_name
