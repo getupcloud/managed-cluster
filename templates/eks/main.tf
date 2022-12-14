19f6a32d604b1bb56bb81b34e693817b52371f9c
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-12-14 18:25:30 -0300

    bump eks

--- templates/eks/main.tf
+++ templates/eks/main.tf
@@ -1,5 +1,5 @@
 module "eks" {
-  source = "github.com/getupcloud/terraform-cluster-eks?ref=v2.0.0-alpha48"
+  source = "github.com/getupcloud/terraform-cluster-eks?ref=v2.0.0-alpha49"
 
   # cluster basics
   customer_name       = var.customer_name
