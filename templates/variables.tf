1dc8bfc649d67d9cad1b6e47c93a7a2980aa4700
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-12-26 14:08:01 -0300

    Always asks opsgenie and cronitor api keys

--- templates/variables.tf
+++ templates/variables.tf
@@ -1,11 +1,9 @@
 variable "cronitor_api_key" {
   description = "Cronitor API key"
   type        = string
-  default     = ""
 }
 
 variable "opsgenie_api_key" {
   description = "Opsgenie API key to create prometheus integration"
   type        = string
-  default     = ""
 }
