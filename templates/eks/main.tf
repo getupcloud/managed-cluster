b3586329531ab375b3706646956505d7990a376e
mmmarceleza <marcelo.melo@protonmail.com>
2023-01-12 19:08:41 -0300

    bump eks

--- templates/eks/main.tf
+++ templates/eks/main.tf
@@ -1,5 +1,5 @@
 module "eks" {
-  source = "github.com/getupcloud/terraform-cluster-eks?ref=v2.0.0-alpha55"
+  source = "github.com/getupcloud/terraform-cluster-eks?ref=v2.0.0-alpha56"
 
   # cluster basics
   customer_name       = var.customer_name
