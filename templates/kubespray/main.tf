ffac77e419946febf7ea94e096e94da346f46171
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2023-01-19 11:36:44 -0300

    kubespray: passing taints/labels vars

--- templates/kubespray/main.tf
+++ templates/kubespray/main.tf
@@ -26,20 +26,26 @@ module "cluster" {
   manifests_template_vars = local.manifests_template_vars
 
   # provider specific
-  api_endpoint        = var.api_endpoint
-  app_nodes           = var.app_nodes
-  deploy_components   = var.deploy_components
-  etc_hosts           = var.etc_hosts
-  infra_nodes         = var.infra_nodes
-  install_packages    = var.install_packages
-  kubeconfig_filename = var.kubeconfig_filename
-  kubespray_git_ref   = var.kubespray_git_ref
-  master_nodes        = var.master_nodes
-  region              = var.region
-  ssh_private_key     = var.ssh_private_key
-  ssh_user            = var.ssh_user
-  ssh_password        = var.ssh_password
-  systemctl_enable    = var.systemctl_enable
-  systemctl_disable   = var.systemctl_disable
-  uninstall_packages  = var.uninstall_packages
+  api_endpoint               = var.api_endpoint
+  app_nodes                  = var.app_nodes
+  default_app_node_labels    = var.default_app_node_labels
+  default_app_node_taints    = var.default_app_node_taints
+  default_infra_node_labels  = var.default_infra_node_labels
+  default_infra_node_taints  = var.default_infra_node_taints
+  default_master_node_labels = var.default_master_node_labels
+  default_master_node_taints = var.default_master_node_taints
+  deploy_components          = var.deploy_components
+  etc_hosts                  = var.etc_hosts
+  infra_nodes                = var.infra_nodes
+  install_packages           = var.install_packages
+  kubeconfig_filename        = var.kubeconfig_filename
+  kubespray_git_ref          = var.kubespray_git_ref
+  master_nodes               = var.master_nodes
+  region                     = var.region
+  ssh_private_key            = var.ssh_private_key
+  ssh_user                   = var.ssh_user
+  ssh_password               = var.ssh_password
+  systemctl_enable           = var.systemctl_enable
+  systemctl_disable          = var.systemctl_disable
+  uninstall_packages         = var.uninstall_packages
 }
