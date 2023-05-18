f8e8f32aad4777b613f51d090be004bfcd89feb9
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2023-05-18 16:09:00 -0300

    bump gke

--- templates/gke/main.tf
+++ templates/gke/main.tf
@@ -1,5 +1,5 @@
 module "gke" {
-  source = "github.com/getupcloud/terraform-cluster-gke?ref=main" ## v1.22
+  source = "github.com/getupcloud/terraform-cluster-gke?ref=v2.0.0"
 
   # cluster basics
   customer_name  = var.customer_name
