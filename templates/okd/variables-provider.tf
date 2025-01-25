78655cf231392ec0dd21b7d226e95699a34fa954
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2025-01-25 00:49:55 +0000

    okd: missing defaults

--- templates/okd/variables-provider.tf
+++ templates/okd/variables-provider.tf
@@ -19,6 +19,7 @@ variable "cluster_monitoring_config" {
     prometheus_k8s_retention_size = optional(string, "100GiB")
     prometheus_k8s_log_level      = optional(string, "info")
   })
+  default={}
 }
 
 variable "user_workload_monitoring_config" {
@@ -35,4 +36,5 @@ variable "user_workload_monitoring_config" {
     alertmanager_enable_alertmanager_config = optional(bool, false)
     alertmanager_log_level                  = optional(string, "info")
   })
+  default={}
 }
