d2541b0596876bc3795d1178fdce5e5a2cf97602
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2026-01-06 14:14:13 +0000

    bump kubespray
    Signed-off-by: Mateus Caruccio <mateus.caruccio@getupcloud.com>

--- templates/kubespray/main.tf
+++ templates/kubespray/main.tf
@@ -1,5 +1,5 @@
 module "cluster" {
-  source = "github.com/getupcloud/terraform-cluster-kubespray?ref=v6.2.17"
+  source = "github.com/getupcloud/terraform-cluster-kubespray?ref=v6.2.18"
 
   # cluster basics
   customer_name      = var.customer_name
